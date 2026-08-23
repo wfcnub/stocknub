625602ee730666f2c5878dc7c878688875fc981a
Bagood <4riabagus@gmail.com>
2026-08-23 09:04:48 +0700

    update daily_forecast and add new service for initializing streamlit

--- daily_forecasts.py
+++ daily_forecasts.py
@@ -93,13 +93,12 @@ def run_step(step_num, args):
         
     if step_num == 0:
         cmd.extend(["--start_date", '2020-01-01'])
-        cmd.extend(["--process_selected_ticker"])
 
     elif step_num == 1:
-        cmd.extend(["--process_selected_ticker"])
+        pass
     
     elif step_num == 2:
-        cmd.extend(["--process_selected_ticker"])
+        pass
 
     elif step_num == 3:
         cmd.extend(["--windows", '5,10'])
