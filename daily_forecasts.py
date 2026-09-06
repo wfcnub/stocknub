e9b50154cfa5df70e067b75cb24d2cbc225548c6
Bagood <4riabagus@gmail.com>
2026-09-06 19:39:59 +0700

    added update to the fetch ohlcv

--- daily_forecasts.py
+++ daily_forecasts.py
@@ -93,10 +93,10 @@ def run_step(step_num, args):
         
     if step_num == 0:
         cmd.extend(["--start_date", '2020-01-01'])
-        cmd.extend("--process_selected_ticker")
+        cmd.extend(["--process_selected_ticker"])
 
     elif step_num == 1:
-        pass
+        cmd.extend(["--process_selected_ticker"])
     
     elif step_num == 2:
         pass
